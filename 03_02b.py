fd377f121431f708568eafaf6080ecc155769b12
marrajj <57151181+marrajj@users.noreply.github.com>
2023-04-08 22:48:10 +0000

    read csv assigned to namedtuple and print email

--- 03_02b.py
+++ 03_02b.py
@@ -1,8 +1,14 @@
-
+from collections import namedtuple
+from csv import reader
 def main():
-    #add code here
     #read data/Customer.csv
+    with open("data/Customer.csv", "r") as open_csv:
+        read = reader(open_csv)
+        Person = namedtuple("Person", next(read))
     #Create workable objects with each line
+        for line in read:
+            person = Person(*line)
+            print(person.Email)
     return
 
 if __name__ == "__main__":
